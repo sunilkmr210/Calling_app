4403f00c386f55bb495750597ec8a1bb4475f984
Sunil <sunilkr15012000@gmail.com>
2024-07-29 17:53:15 +0530

    forth

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -6,29 +6,33 @@ const SocketHandler = (req, res) => {
         console.log('socket is already running')
     }
     else {
-        const io = new Server(res.socket.server);
+        const io = new Server(res.socket.server, {
+            cors: {
+                origin: "https://shark-app-6nv3k.ondigitalocean.app"
+            }
+        });
         res.socket.server.io = io;
 
         io.on('connection', (socket) => {
             console.log('Server is connected');
 
-            socket.on('join-room', (roomId, userId)=>{
+            socket.on('join-room', (roomId, userId) => {
                 console.log(`a new user ${userId} joined room ${roomId}`)
                 socket.join(roomId);
                 socket.broadcast.to(roomId).emit('user-connected', userId);
             });
 
-            socket.on('user-toggle-audio', (userId, roomId)=>{
+            socket.on('user-toggle-audio', (userId, roomId) => {
                 //no need for socket.join but no duplicates are formed 
                 socket.join(roomId);
                 socket.broadcast.to(roomId).emit('user-toggle-audio', userId);
             })
-            socket.on('user-toggle-video', (userId, roomId)=>{
+            socket.on('user-toggle-video', (userId, roomId) => {
                 socket.join(roomId);
                 socket.broadcast.to(roomId).emit('user-toggle-video', userId);
             })
 
-            socket.on('user-leave', (userId, roomId)=>{
+            socket.on('user-leave', (userId, roomId) => {
 
                 socket.join(roomId);
                 socket.broadcast.to(roomId).emit('user-leave', userId);
