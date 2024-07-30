2e57818ee0a57f72f396b988633d4d987637333d
Sunil <sunilkr15012000@gmail.com>
2024-07-30 08:58:10 +0530

    3

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -8,7 +8,7 @@ const SocketHandler = (req, res) => {
     else {
         const io = new Server(res.socket.server, {
             cors: {
-                origin: 'https://shark-app-6nv3k.ondigitalocean.app/',
+                origin: 'https://shark-app-6nv3k.ondigitalocean.app/:8080',
                 methods: ["GET", "POST"],
                 credentials: true
             },
